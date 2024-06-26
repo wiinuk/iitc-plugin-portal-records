7f7604cd82cea499f7f0fa78ad7701550e42e94a
wiinuk <wii.nukegara@gmail.com>
2024-06-26 16:53:16 +0900

    修正

--- source/s2-geometry.d.ts
+++ source/s2-geometry.d.ts
@@ -1,11 +1,16 @@
 // spell-checker: ignore lngs
 declare namespace S2 {
-    export namespace S2Cell {
-        export function FromLatLng(
-            latLng: Readonly<S2LatLng>,
-            level: number
-        ): S2Cell;
-        export function FromFaceIJ(
+    export class S2Cell {
+        readonly face: number;
+        readonly ij: readonly [number, number];
+        readonly level: number;
+        getLatLng(): S2LatLng;
+        getCornerLatLngs(): [S2LatLng, S2LatLng, S2LatLng, S2LatLng];
+        getNeighbors(): [S2Cell, S2Cell, S2Cell, S2Cell];
+        toString(): string;
+
+        static FromLatLng(latLng: Readonly<S2LatLng>, level: number): S2Cell;
+        static FromFaceIJ(
             face: number,
             ij: readonly [number, number],
             level: number
@@ -15,13 +20,4 @@ declare namespace S2 {
 interface S2LatLng {
     lat: number;
     lng: number;
-}
-interface S2Cell {
-    readonly face: number;
-    readonly ij: readonly [number, number];
-    readonly level: number;
-    getLatLng(): S2LatLng;
-    getCornerLatLngs(): [S2LatLng, S2LatLng, S2LatLng, S2LatLng];
-    getNeighbors(): [S2LatLng, S2LatLng, S2LatLng, S2LatLng];
-    toString(): string;
 }
