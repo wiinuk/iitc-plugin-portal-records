47cbc42f476e990f576376f2112cdae56b35fec1
wiinuk <wii.nukegara@gmail.com>
2024-06-26 21:27:03 +0900

    修正

--- source/s2-geometry.d.ts
+++ source/s2-geometry.d.ts
@@ -1,6 +1,6 @@
 // spell-checker: ignore lngs
 declare namespace S2 {
-    export class S2Cell {
+    export interface S2CellLike {
         readonly face: number;
         readonly ij: readonly [number, number];
         readonly level: number;
@@ -8,7 +8,8 @@ declare namespace S2 {
         getCornerLatLngs(): [S2LatLng, S2LatLng, S2LatLng, S2LatLng];
         getNeighbors(): [S2Cell, S2Cell, S2Cell, S2Cell];
         toString(): string;
-
+    }
+    export class S2Cell extends S2CellLike {
         static FromLatLng(latLng: Readonly<S2LatLng>, level: number): S2Cell;
         static FromFaceIJ(
             face: number,
